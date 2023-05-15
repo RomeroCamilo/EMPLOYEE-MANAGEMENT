c5f1ae39efa410e599828f47289a2cfc4b19007d
Camilo Romero <91925901+RomeroCamilo@users.noreply.github.com>
2023-05-15 16:23:09 -0400

    delete employee

--- final-frontend-main/src/components/views/AllIEmployeesView.js
+++ final-frontend-main/src/components/views/AllIEmployeesView.js
@@ -1,35 +1,42 @@
+import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-
-
 const AllEmployeesView = (props) => {
-  if (!props.allEmployees.length) {
+  const { allEmployees, deleteEmployee } = props;
+
+  if (!allEmployees.length) {
     return <div>There are no employees.</div>;
   }
 
+  const handleDelete = (employeeId) => {
+    deleteEmployee(employeeId);
+  };
+
   return (
     <div>
- 
-      <Link to={`/`}><h1>HOME</h1></Link>
-      {props.allEmployees.map((employee) => {
+      <Link to={`/`}>
+        <h1>HOME</h1>
+      </Link>
+      {allEmployees.map((employee) => {
         let name = employee.firstname + " " + employee.lastname;
         return (
           <div key={employee.id}>
-          <Link to={`/employee/${employee.id}`}>
-            <h1>{name}</h1>
-          </Link>
-          <p>{employee.department}</p>
-        </div>
+            <Link to={`/employee/${employee.id}`}>
+              <h1>{name}</h1>
+            </Link>
+            <p>{employee.department}</p>
+            <button onClick={() => handleDelete(employee.id)}>Delete</button>
+          </div>
         );
       })}
     </div>
   );
 };
 
-
 AllEmployeesView.propTypes = {
   allEmployees: PropTypes.array.isRequired,
+  deleteEmployee: PropTypes.func.isRequired,
 };
 
 export default AllEmployeesView;
